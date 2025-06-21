0923cabd1c7731f88d0f751bb6e5970915f2c895
sander <sandergs@protonmail.com>
2025-06-21 13:34:12 +0200

    Add i3 and i3status config

--- install.sh
+++ install.sh
@@ -12,3 +12,11 @@ ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim"
 ########
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+########
+#  i3  #
+########
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+rm -rf "$HOME/.config/i3status"
+ln -s "$HOME/dotfiles/i3status" "$HOME/.config"
